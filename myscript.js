14a2654529343b17591d09a70d246f582cd992d9
davidemontagna <davidemontagna92@gmail.com>
2021-12-22 17:29:43 +0100

    Esercizio completato

--- myscript.js
+++ myscript.js
@@ -9,38 +9,61 @@ let numGenerator = document.getElementById("dm_button");
 
 numGenerator.addEventListener("click", function(){
     let numArr = []; //array contentente i numeri da far visualizzare
-
+    const showResult = document.getElementById("dm_result");
     const showNumbers = document.getElementById("dm_bg_num");
     showNumbers.innerHTML = "";
-
+    
     //ciclo per pushare i numeri nell'array
     for (i=0; i<5; i++){
         numArr.push(numsCreator());
-    }
-    //console.log(numArr)
-    
+    }    
     showNumbers.innerHTML += `<span class="dm_numbers"> ${numArr} </span>`
 
-    
     /* Dopo tot secondi ("o meglio, millisecondi") parte la funzione
     per richiedere all'utente di inserire i numeri */
-    setTimeout(askNumbers, 3000);
+    let userNumArr = [];
+    
+    setTimeout(deleteNums, 30000)
+    setTimeout(askNumbers, 30500, numArr, userNumArr, showResult)
+    
+    //let counter = rightNum(numArr, userNumArr, showResult);
     
 })
 
+function rightNum(numArr, userNumArr, showResult){
+    let count = 0;
+    let countRights = [];
+    for (i=0; i<5; i++){
+        if(numArr.includes(userNumArr[i])){
+            count++;
+            countRights.push(userNumArr[i]);
+        }        
+    }
 
+    if(count>0){
+        showResult.innerHTML = `<h2 class="text-white">Hai inserito ${count} numero/i esatti: ${countRights}</h2>`;
+    }else{
+        showResult.innerHTML = `<h2 class="text-white">Non hai inserito nessun numero corretto</h2>`;
+    }
+    
+}
 
-function askNumbers(){
-    //creo un array per contenere i numeri inseriti dall'utente
-    let userNumArr = [];
+//funzione per far sparire i numeri
+function deleteNums(){
+    const showNumbers = document.getElementById("dm_bg_num");
+    showNumbers.innerHTML = "";
+}
+
+//funzione per fare inserire i numeri all'utente
+function askNumbers(numArr, userNumArr, showResult){
     const positions = ["primo", "secondo", "terzo", "quarto", "quinto"];
     for (i=0; i<5; i++){
-        userNumArr.push(prompt(`Inserisci il  ${positions[i]} numero`));
+        userNumArr.push(parseInt(prompt(`Inserisci il  ${positions[i]} numero`)));
         
     }
     console.log(userNumArr);
+    rightNum(numArr, userNumArr, showResult);
 }
-
 
 //funzione per generare i numeri
 function numsCreator(){
